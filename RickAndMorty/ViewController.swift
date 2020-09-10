f00eb00f6a30ad98435f0fecd0de6d44c804afb3
sagintayev <sagintayevzh@yandex.kz>
2020-09-10 14:28:59 +0600

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Askhat Zhabayev on 2020-09-10.
+//  Copyright © 2020 Zhanibek Sagintayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
