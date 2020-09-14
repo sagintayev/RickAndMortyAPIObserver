16b21ba33bc8973111b37c970cdc8504ceffd611
sagintayev <sagintayevzh@yandex.kz>
2020-09-14 14:54:27 +0600

    Create Extensions.swift and add date decoding strategy extension

---
+++ RickAndMorty/Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Extensions.swift
+//  RickAndMorty
+//
+//  Created by Askhat Zhabayev on 2020-09-14.
+//  Copyright © 2020 Zhanibek Sagintayev. All rights reserved.
+//
+
+import Foundation
+
+extension Formatter {
+    static let iso8601withFractionalSeconds: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+}
+
+extension JSONDecoder.DateDecodingStrategy {
+    static let iso8601withFractionalSeconds = custom {
+        let container = try $0.singleValueContainer()
+        let string = try container.decode(String.self)
+        guard let date = Formatter.iso8601withFractionalSeconds.date(from: string) else {
+            throw DecodingError.dataCorruptedError(in: container,
+                  debugDescription: "Invalid date: " + string)
+        }
+        return date
+    }
+}
