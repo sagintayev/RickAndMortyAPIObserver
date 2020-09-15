95a3e4585ddf688df47ab7b70d0b90770c95f80f
sagintayev <sagintayevzh@yandex.kz>
2020-09-15 23:27:28 +0600

    Implement obtaining resources from specific page

--- RickAndMorty/Models/GettableFromAPIProtocol.swift
+++ RickAndMorty/Models/GettableFromAPIProtocol.swift
@@ -17,6 +17,7 @@ protocol GettableFromAPI: Codable {
     
     static func getAll(completion: @escaping ([Resource]) -> Void)
     static func getByID(_ id: Int, completion: @escaping (Resource) -> Void)
+    static func getByPageNumber(_ number: Int, completion: @escaping ([Resource]) -> Void)
 }
 
 extension GettableFromAPI {
@@ -30,6 +31,17 @@ extension GettableFromAPI {
             if let data = data {
                 if let resource = try? getDecoder().decode(Resource.self, from: data) {
                     completion(resource)
+                }
+            }
+        }
+    }
+    
+    static func getByPageNumber(_ number: Int, completion: @escaping (([Resource]) -> Void)) {
+        networkHandler.getByPartOfURL("\(resourceName)/?page=\(number)") { (data, error) in
+            if let data = data {
+                if let resources = try? getDecoder().decode(ResourcesWithServiceInfo<Resource>.self, from: data) {
+                    print(resources.info)
+                    completion(resources.results)
                 }
             }
         }
