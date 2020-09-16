5cb1e4b4626c4d07bbed88d1b1834de80998b3b8
sagintayev <sagintayevzh@yandex.kz>
2020-09-16 15:11:27 +0600

    Create view controller for feed of characters

---
+++ RickAndMorty/ViewControllers/CharacterFeedVC.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterFeedVC.swift
+//  RickAndMorty
+//
+//  Created by Askhat Zhabayev on 2020-09-16.
+//  Copyright © 2020 Zhanibek Sagintayev. All rights reserved.
+//
+
+import UIKit
+
+class CharacterFeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
