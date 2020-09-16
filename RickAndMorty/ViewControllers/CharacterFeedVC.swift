fb55f21d231e71340a09cd4411898ee9207d2db7
sagintayev <sagintayevzh@yandex.kz>
2020-09-16 15:21:33 +0600

    Create collection view and make view controller to be its delegate and data source

--- RickAndMorty/ViewControllers/CharacterFeedVC.swift
+++ RickAndMorty/ViewControllers/CharacterFeedVC.swift
@@ -9,22 +9,36 @@
 import UIKit
 
 class CharacterFeedVC: UIViewController {
+    
+    var characterCollection: UICollectionView {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view = characterCollection
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension CharacterFeedVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
-    */
+    
+    
+}
 
+extension CharacterFeedVC: UICollectionViewDelegate {
+    
 }
