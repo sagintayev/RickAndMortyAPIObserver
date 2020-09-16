493dba0e726e60299bbf375690f5f2d2ad790438
sagintayev <sagintayevzh@yandex.kz>
2020-09-16 15:07:55 +0600

    Create custom UITabBarController and make it root view controller

--- RickAndMorty/SceneDelegate.swift
+++ RickAndMorty/SceneDelegate.swift
@@ -16,10 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let viewController = ViewController()
+        let rootVC = TabBarController()
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = viewController
+        window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
     }
---
+++ RickAndMorty/ViewControllers/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabViewController.swift
+//  RickAndMorty
+//
+//  Created by Askhat Zhabayev on 2020-09-16.
+//  Copyright © 2020 Zhanibek Sagintayev. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
