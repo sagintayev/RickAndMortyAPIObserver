856f076aa9ea4446cbb0159565383e2106c20906
sagintayev <sagintayevzh@yandex.kz>
2020-09-20 13:25:04 +0600

    Add character model to CharacterFeedVC

--- RickAndMorty/ViewControllers/CharacterFeedVC.swift
+++ RickAndMorty/ViewControllers/CharacterFeedVC.swift
@@ -9,33 +9,69 @@
 import UIKit
 
 class CharacterFeedVC: UIViewController {
+    let spacingBetweenItems = CGFloat(10)
+    let itemsPerRow = CGFloat(3)
     
-    var characterCollection = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var characters: [Character]?
     
-    override func loadView() {
-        super.loadView()
-        view = characterCollection
-    }
+    var characterCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemGray5
+        collectionView.register(CharacterCell.self, forCellWithReuseIdentifier: CharacterCell.identifier)
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(characterCollection)
+        characterCollection.frame = view.bounds
         characterCollection.delegate = self
         characterCollection.dataSource = self
+        Character.getAll { (characters) in
+            DispatchQueue.main.async {
+                self.characters = characters
+                self.characterCollection.reloadData()
+            }
+        }
     }
 }
 
 extension CharacterFeedVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return characters?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCell.identifier, for: indexPath)
+        if let cell = cell as? CharacterCell {
+            let characterID = indexPath.item + 1
+            Character.getByID(characterID) { (character) in
+                cell.tag = characterID
+                character.getImage { (imageData) in
+                    if characterID == cell.tag {
+                        cell.image = UIImage(data: imageData)
+                        cell.labelText = character.name
+                    }
+                }
+            }
+        }
+        return cell
     }
-    
-    
 }
 
 extension CharacterFeedVC: UICollectionViewDelegate {
     
+}
+
+extension CharacterFeedVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.bounds.width - itemsPerRow * spacingBetweenItems) / itemsPerRow
+        let height = width
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacingBetweenItems
+    }
 }
