8f5f4fab859488765d9ce4bf3a46729d812c9605
sagintayev <sagintayevzh@yandex.kz>
2020-09-20 14:04:54 +0600

    Create view controller to show detail information about Character

---
+++ RickAndMorty/ViewControllers/CharacterDetailVC.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterDetailVC.swift
+//  RickAndMorty
+//
+//  Created by Askhat Zhabayev on 2020-09-20.
+//  Copyright © 2020 Zhanibek Sagintayev. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
