18b8a880660ec22567f3763e3dbce4d93710b836
sagintayev <sagintayevzh@yandex.kz>
2020-09-30 10:39:12 +0600

    Add season and episode vars to Episode model

--- RickAndMorty/Models/Episode.swift
+++ RickAndMorty/Models/Episode.swift
@@ -17,6 +17,17 @@ struct Episode: Codable {
     let airDate: Date
     let characters: [URL]
     
+    var season: Int {
+        let firstIndex = code.index(code.firstIndex(of: "S")!, offsetBy: 1)
+        let secondIndex = code.firstIndex(of: "E")!
+        return Int(String(code[firstIndex..<secondIndex]))!
+    }
+    var episode: Int {
+        let firstIndex = code.index(code.firstIndex(of: "E")!, offsetBy: 1)
+        let secondIndex = code.endIndex
+        return Int(String(code[firstIndex..<secondIndex]))!
+    }
+    
     private enum CodingKeys: String, CodingKey  {
         case id, name, created, url, characters
         case code = "episode"
@@ -28,4 +39,32 @@ extension Episode: GettableFromAPI {
     typealias Resource = Self
     static var resourceName = "episode"
     static var networkHandler = NetworkHandler()
+}
+
+struct EpisodesDividedBySeasons {
+    var seasons: Int {
+        return episodes.keys.count
+    }
+    
+    subscript(index: Int) -> [Episode]? {
+        get {
+            return episodes[index]
+        }
+        set {
+            episodes[index] = newValue
+        }
+    }
+    
+    init(_ episodes: [Episode]) {
+        for episode in episodes {
+            let season = episode.season
+            if self.episodes.keys.contains(season) {
+                self.episodes[season]!.append(episode)
+            } else {
+                self.episodes[season] = [episode]
+            }
+        }
+    }
+    
+    private var episodes = [Int: [Episode]]()
 }
