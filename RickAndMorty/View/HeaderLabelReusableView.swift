2570db7f1e9f6d353652203f3f8b1d2574767296
sagintayev <sagintayevzh@yandex.kz>
2020-10-28 14:20:30 +0600

    Create HeaderLabelReusableView

--- RickAndMorty/View/HeaderLabelReusableView.swift
+++ RickAndMorty/View/HeaderLabelReusableView.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class HeaderLabelReusableView: UICollectionReusableView {
-        
+    // Properties
+    static let identifier = "header-label-reusable-view"
+    var labelText: String? {
+        didSet {
+            headerLabel.text = labelText
+        }
+    }
+    private var headerLabel: UILabel = {
+       let label = UILabel()
+        return label
+    }()
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        headerLabel.embedIn(self)
+    }
 }
